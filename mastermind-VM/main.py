adc6eda0fb9e1dcf328cb4a5fbcda3fa6d9a42f2
frans <fransdauphin@gmail.com>
2023-08-15 10:53:04 +0200

    nickname box does post

--- mastermind-VM/main.py
+++ mastermind-VM/main.py
@@ -1,7 +1,9 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, session, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
+    # nickname = request.form['nickname_form']
+    # session['nickname'] = nickname
     return render_template('index.html')
