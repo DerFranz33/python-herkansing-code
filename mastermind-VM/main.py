f1e8de117861d8790d8cb30fa25fc4f8f838d849
frans <fransdauphin@gmail.com>
2023-08-15 11:10:42 +0200

    use of session for nickname

--- mastermind-VM/main.py
+++ mastermind-VM/main.py
@@ -2,16 +2,19 @@ from flask import Flask, url_for, render_template, session, request, redirect
 
 app = Flask(__name__)
 
+app.secret_key = 'VeryHard@ToGuess666SecretKey#'
+
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    # nickname = request.form['nickname_form']
-    # session['nickname'] = nickname
     if(request.method == 'POST'):
         nickname =  request.form['nickname']
-        return redirect(url_for('game', nick_name=nickname))
+        session['nickname'] = nickname
+        return redirect(url_for('game'))
     else:
         return render_template('index.html')
     
-@app.route('/game/<nick_name>')
-def game(nick_name):
-    return f'<p>{nick_name}</p>'
+@app.route('/game/')
+def game():
+    if('nickname' in session):
+        nickname = session['nickname']
+    return f'<p>{nickname}</p>'
