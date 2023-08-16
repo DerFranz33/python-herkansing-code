a5a30e52399a020e16efdcdeedfa901f5368d3c9
frans <fransdauphin@gmail.com>
2023-08-16 20:25:28 +0200

    database problem fix -- some code uncommented

--- mastermind-VM/main.py
+++ mastermind-VM/main.py
@@ -2,19 +2,45 @@ from flask import Flask, url_for, render_template, session, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import re as regex
 
+
+# create the extension
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
-app.secret_key = 'VeryHard@ToGuess666SecretKey#'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-db = SQLAlchemy(app)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
+app.secret_key = 'ali'
 
+# initialize the app with the extension
+db.init_app(app)
 
-class players(db.Model):
+class Players(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(100), unique=True)
 
-    def __init__(self, nickname):
-        self.nickname = nickname
+with app.app_context():
+    db.create_all()
+
+# from flask import Flask, url_for, render_template, session, request, redirect
+# from flask_sqlalchemy import SQLAlchemy
+# import re as regex
+
+# db = SQLAlchemy()
+# app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# db.init_app(app)
+
+# with app.app_context():
+#         db.create_all()
+
+
+
+# class Players(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+    # nickname = db.Column(db.String(100), unique=True)
+
+
 
 
 
@@ -25,7 +51,8 @@ def home():
         nickname =  request.form['nickname']
         session['nickname'] = nickname
         # Add player
-        temp_player = players(nickname)
+        temp_player = Players()
+        temp_player.nickname = nickname
         db.session.add(temp_player)
         db.session.commit()
         return redirect(url_for('game'))
@@ -46,24 +73,24 @@ def game():
     else:
         return redirect(url_for('home'))
     
-@app.route('/reset_nickname')
-def reset_nickname():
-    session.pop('nickname', None)
-    return redirect(url_for('home'))
+# @app.route('/reset_nickname')
+# def reset_nickname():
+#     session.pop('nickname', None)
+#     return redirect(url_for('home'))
 
-@app.route('/statistics')
-def statistics():
-    if __nickname_okay():
-        return render_template('statistics.html', nickname=session['nickname'], values=players.query.all())
-    return redirect(url_for('home'))
+# @app.route('/statistics')
+# def statistics():
+#     if __nickname_okay():
+#         return render_template('statistics.html', nickname=session['nickname'], values=players.query.all())
+#     return redirect(url_for('home'))
 
-def __nickname_okay():
-    if 'nickname' in session:
-        if regex.search('[a-zA-Z]', session['nickname']) != None:
-            return True
-    return False
+# def __nickname_okay():
+#     if 'nickname' in session:
+#         if regex.search('[a-zA-Z]', session['nickname']) != None:
+#             return True
+#     return False
 
 
-if __name__ == '__main__':
-    db.create_all()
-    app.run()
+# if __name__ == '__main__':
+    
+#     app.run()
